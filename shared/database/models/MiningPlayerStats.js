5cc8b6c197c26dfc52d4c31ddd117510817ab58a
axtan64 <pigeon7846@gmail.com>
2026-09-01 19:10:35 +0100

    Hero Level Attributes

--- shared/database/models/MiningPlayerStats.js
+++ shared/database/models/MiningPlayerStats.js
@@ -1,6 +1,6 @@
 const { createPlayerStatsModel } = require('./typedStats');
 
-const STAT_FIELDS = ['level', 'gold', 'highestLevel', 'selectedLevel', 'progress', 'totalBlocksMined', 'heroIndex'];
+const STAT_FIELDS = ['level', 'gold', 'highestLevel', 'selectedLevel', 'progress', 'totalBlocksMined', 'heroIndex', 'heroLevels'];
 const MiningPlayerStats = createPlayerStatsModel('miningPlayerStats', STAT_FIELDS);
 
 module.exports = { MiningPlayerStats, STAT_FIELDS };
---
+++ shared/database/prisma/migrations/20260901000000_add_hero_levels/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "MiningPlayerStats" ADD COLUMN     "heroLevels" JSONB NOT NULL DEFAULT '{}';
